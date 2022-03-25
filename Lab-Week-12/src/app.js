518f0737c9380cb2bbc5a679e92912b6e19dc44a
BennyCao <caobenny122@gmail.com>
2022-03-24 23:32:22 -0700

    added js link and question 2

---
+++ Lab-Week-12/src/app.js
@@ -0,0 +1,50 @@
+function selectSomething(){
+    const selectEle = document.querySelector('div')
+    return selectEle
+}
+console.log(selectSomething())
+
+// part 2: Creating a new array with different keys based on another array
+
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+]
+const studentsScholarship = []
+
+function getAvg(student){
+    let totalGrade = 0
+    for (let grade of student.grades) {
+        totalGrade += grade
+    }
+    return gradeTotal
+}
+function NewList(student, gradeAvg){
+    if (gradeAvg >= 80){
+        studentsScholarship.push({
+            name: student.name, gradeAvg: gradeAvg
+        })
+    }
+}
+function makeList(studentList){
+    let gradeAvg = 0
+    for (let student of studentList) {
+        let gradeAvg = getAvg(student) / student.grades.length
+        addToNewList(student, gradeAvg)
+    }
+    return studentsScholarship
+}
+
+makeList(students)
+
+console.log(studentsScholarship)
+
+
+function darkmode(){
+    const darkm = document.querySelector("div");
+    darkm.classList.replace("light-theme", 'dark-theme');
+}
+
+darkmode()
